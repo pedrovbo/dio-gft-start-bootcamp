42a69331824c269b06f9281bae06798e0f1bd294
pedrovbo <pedrovicboliveira@gmail.com>
2022-02-13 18:04:49 -0300

    implemented client controller

--- digital-bank/src/main/java/com/api/digitalbank/DigitalBankApplication.java
+++ digital-bank/src/main/java/com/api/digitalbank/DigitalBankApplication.java
@@ -2,13 +2,20 @@ package com.api.digitalbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class DigitalBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalBankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Olá Mundo!";
+    }
 
 }
--- digital-bank/src/main/java/com/api/digitalbank/controllers/ClientController.java
+++ digital-bank/src/main/java/com/api/digitalbank/controllers/ClientController.java
@@ -1,4 +1,85 @@
 package com.api.digitalbank.controllers;
 
+
+import com.api.digitalbank.dtos.ClientDto;
+import com.api.digitalbank.models.ClientModel;
+import com.api.digitalbank.services.ClientService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/client")
 public class ClientController {
+
+    final ClientService clientService;
+
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveClient(@RequestBody @Valid ClientDto clientDto){
+        if (clientService.existsByCpf(clientDto.getCpf())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: CPF is already registered!");
+        }
+        if (clientService.existsByName(clientDto.getName())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Name is already registered!");
+        }
+
+        var clientModel = new ClientModel();
+        BeanUtils.copyProperties(clientDto, clientModel);
+        // reminder //
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(clientModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientModel>> getAllClients(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneClient(@PathVariable(value = "id") UUID id){
+        Optional<ClientModel> clientModelOptional = clientService.findById(id);
+        if (!clientModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(clientModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteClient(@PathVariable(value = "id") UUID id){
+        Optional<ClientModel> clientModelOptional = clientService.findById(id);
+        if(!clientModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found");
+        }
+        clientService.delete(clientModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Client deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateClient(@PathVariable(value = "id") UUID id,
+                                                    @RequestBody @Valid ClientDto clientDto){
+        Optional<ClientModel> clientModelOptional = clientService.findById(id);
+        if(!clientModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found.");
+        }
+
+        var clientModel = new ClientModel();
+        BeanUtils.copyProperties(clientDto, clientModel);
+        clientModel.setId(clientModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.save(clientModel));
+    }
+
+
 }
