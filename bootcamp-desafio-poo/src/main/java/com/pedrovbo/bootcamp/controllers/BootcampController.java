7d5da3cf53a5e0411c73b995116faf0a23945602
pedrovbo <pedrovicboliveira@gmail.com>
2022-02-17 18:29:06 -0300

    implemented Post Method for Bootcamp class

---
+++ bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/controllers/BootcampController.java
@@ -0,0 +1,34 @@
+package com.pedrovbo.bootcamp.controllers;
+
+import com.pedrovbo.bootcamp.dtos.BootcampDto;
+import com.pedrovbo.bootcamp.dtos.DevDto;
+import com.pedrovbo.bootcamp.model.Bootcamp;
+import com.pedrovbo.bootcamp.model.Dev;
+import com.pedrovbo.bootcamp.services.BootcampService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/bootcamp")
+public class BootcampController {
+
+    final BootcampService bootcampService;
+
+    public BootcampController(BootcampService bootcampService) {
+        this.bootcampService = bootcampService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveBootcamp(@RequestBody @Valid BootcampDto bootcampDto){
+        var bootcamp = new Bootcamp();
+        BeanUtils.copyProperties(bootcampDto, bootcamp);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(bootcampService.save(bootcamp));
+    }
+
+}
---
+++ bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/dtos/BootcampDto.java
@@ -0,0 +1,36 @@
+package com.pedrovbo.bootcamp.dtos;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+public class BootcampDto {
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+}
