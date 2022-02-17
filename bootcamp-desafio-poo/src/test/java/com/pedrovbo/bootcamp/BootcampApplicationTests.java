ef9293169ecc3e150f5fd31ced9d877e64d16bd1
pedrovbo <pedrovicboliveira@gmail.com>
2022-02-17 08:44:25 -0300

    add new project Bootcamp Desafio POO

---
+++ bootcamp-desafio-poo/src/test/java/com/pedrovbo/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrovbo.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
