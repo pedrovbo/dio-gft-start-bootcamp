52dfba26607fd71918b111f27d9d2e9e30ccd304
pedrovbo <pedrovicboliveira@gmail.com>
2022-02-17 16:55:21 -0300

    removed abstract class Conteudo

--- bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/model/Curso.java
+++ bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/model/Curso.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 @Table(name = "TB_CURSO")
 @SequenceGenerator(name="CUR_SEQ", sequenceName = "CURSO_SEQ",
     initialValue = 1, allocationSize = 1)
-public class Curso extends Conteudo implements Serializable {
+public class Curso implements Serializable {
 
     private static final long serialVersionUID
             = -4023522856316087762L;
@@ -18,6 +18,13 @@ public class Curso extends Conteudo implements Serializable {
     private Long id;
     @Column(name = "carga_horaria", nullable = false)
     private int cargaHoraria;
+
+    @Column
+    protected static final double XP_PADRAO = 10d;
+    @Column(nullable = false, length = 20)
+    private String titulo;
+    @Column(length = 100)
+    private String descricao;
 
     //TODO: Pesquisar como implementar os atributos da interface Conteudo no Banco de Dados
 
--- bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/model/Mentoria.java
+++ bootcamp-desafio-poo/src/main/java/com/pedrovbo/bootcamp/model/Mentoria.java
@@ -1,13 +1,14 @@
 package com.pedrovbo.bootcamp.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "TB_MENTORIA")
 @SequenceGenerator(name = "MEN_SEQ", sequenceName = "MENTORIA_SEQ",
     initialValue = 1, allocationSize = 1)
-public class Mentoria extends Conteudo {
+public class Mentoria implements Serializable {
 
     private static final long serialVersionUID
             = -4023522856316087762L;
@@ -19,6 +20,13 @@ public class Mentoria extends Conteudo {
 
     @Column
     private LocalDate data;
+
+    @Column
+    protected static final double XP_PADRAO = 10d;
+    @Column(nullable = false, length = 20)
+    private String titulo;
+    @Column(length = 100)
+    private String descricao;
 
     public LocalDate getData() {
         return data;
