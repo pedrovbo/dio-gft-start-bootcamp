f6419a218cb28d585a8dd763624c71bedfad7e06
pedrovbo <pedrovicboliveira@gmail.com>
2022-02-24 09:27:34 -0300

    add new project - emprestimo-livros

---
+++ emprestimo-livros/src/main/java/com/pedrovbo/emprestimolivros/EmprestimoLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.pedrovbo.emprestimolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoLivrosApplication.class, args);
+	}
+
+}
---
+++ emprestimo-livros/src/test/java/com/pedrovbo/emprestimolivros/EmprestimoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrovbo.emprestimolivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
